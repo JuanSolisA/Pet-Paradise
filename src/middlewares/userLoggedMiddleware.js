7399fddeb5e34e5fe3152610e9283da5d07cff1d
JuanSolisA <juan.solis.trabajo@gmail.com>
2022-11-14 20:59:09 -0300

    prueba sacando userloggedMiddleware

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -1,4 +1,4 @@
-const User = require("../modelsJson/User");
+/* const User = require("../modelsJson/User");
 
 function userLoggedMiddleware(req, res, next) {
   res.locals.isLogged = false;
@@ -20,3 +20,4 @@ function userLoggedMiddleware(req, res, next) {
 }
 
 module.exports = userLoggedMiddleware;
+ */
