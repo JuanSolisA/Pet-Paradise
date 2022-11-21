63d51543a7933f6a378e42aa4248a475657a3926
JuanSolisA <juan.solis.trabajo@gmail.com>
2022-11-21 20:02:27 -0300

    cambiando host

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: "root",
+    username: "desarrollo",
     password: null,
     database: "petparadise_db",
-    host: "localhost",//127.0.0.1
+    host: "127.0.0.1",
     dialect: "mysql",
   }
 };
